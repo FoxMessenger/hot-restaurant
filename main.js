336d69f40fd69e91a891cc59d5a468cac8064c3e
foxmessenger <iamliamfox@gmail.com>
2017-04-22 12:04:54 -0500

    running local host working

--- main.js
+++ main.js
@@ -15,102 +15,153 @@
 // =================
 // DEPENDENCIES
 // =================
-var mysql = require('mysql');
-var bodyParser = require('body-parser');
-var express = require('express');
-var path = require('path');
-
+var fs = require('fs');
+var mysql = require('mysql');				// the database
+var bodyParser = require('body-parser');	// body-parser extract the entire body portion of an incoming request stream
+var express = require('express');			//
+var path = require('path');					//
+var url = require('url');					
+var http = require("http");					
 var app = express();
-	// process environment
-var PORT = process.env.PORT || 3306;
+var PORT = 3300; 		// process environment
+
+// =================
+// DEPENDENCIES
+// =================
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res){
+	
+	var urlParts = url.parse(req.url);
+	
+
+	switch (urlParts.pathname) {
+		
+		case "/":
+			fs.readFile("index.html", function(err, data) {
+				res.writeHead(200, { "Content-Type": "text/html" });	
+				res.end(data);
+			});
+		break;
+
+		case '/reserve':
+			fs.readFile('reserve.html', function(err, data) {
+				res.writeHead(200, {'Content-Type': 'text/html'});	
+				res.end(data);
+			});
+
+		break;
+
+		case '/tables':
+			fs.readFile('table.html', function(err, data) {
+				res.writeHead(200, {'Content-Type': 'text/html'});	
+				res.end(data);
+			});
+
+		break;
+
+		default:
+			fs.readFile('index.html', function(err, data) {
+				res.writeHead(200, {'Content-Type': 'text/html'});	
+				res.end(data);
+			})
+	}
+
+}
+server.listen(PORT, function() {
+	console.log('server is listening to PORT: ' + PORT);
+});
+
 
 // connecting to the database
 var connection = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
+    port: 3300,
     user: 'root',
     password: '',
     database: 'Hot_restaurantDB'
 });
 
 // connected to the server
-connection.connect(function(err) {
-    if (err){
-    	console.log('the error is' + err);
-	} else {
-	    console.log('connected...');
-	}
-});
+// connection.connect(function(err) {
+//     if (err){
+//     	console.log('the error is' + err);
+// 	} else {
+// 	    console.log('connected...');
+// 	}
+// });
 
-// ending the connection
-var end = function() {
-    connection.end(function(err) {
-        // The connection is terminated now
-    });
-}
+// // ending the connection
+// var end = function() {
+//     connection.end(function(err) {
+//         // The connection is terminated now
+//     });
+// }
 
 // ==============================================
 // Sets up the Express app to handle data parsing
 // ==============================================
-app.use(bodyParser.json());
-// I don't know what this does
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// app.use(bodyParser.json());
+// // I don't know what this does
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
-// ==============================================
-// DATA
-// ==============================================
+// // ==============================================
+// // DATA
+// // ==============================================
 
-var forms = [{}];
+// var forms = [{}];
 
-// ==============================================
-// ROUTES
-// ==============================================
+// // ==============================================
+// // ROUTES
+// // ==============================================
 
-// Basic route that sends the user first to the AJAX Page
+// // Basic route that sends the user first to the AJAX Page
 
 
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
 
-app.get("/reserve", function(req, res) {
-  res.sendFile(path.join(__dirname, "reserve.html"));
-});
+// app.get("/reserve", function(req, res) {
+//   res.sendFile(path.join(__dirname, "reserve.html"));
+// });
 
-app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "tables.html"));
-});
+// app.get("/tables", function(req, res) {
+//   res.sendFile(path.join(__dirname, "tables.html"));
+// });
 
-// Search for Specific Character (or all characters) - provides JSON
-app.get("/api/:forms?", function(req, res) {
-  var chosen = req.params.forms;
+// // Search for Specific Character (or all characters) - provides JSON
+// app.get("/api/:forms?", function(req, res) {
+//   var chosen = req.params.forms;
 
-  if (chosen) {
-    console.log(chosen);
+//   if (chosen) {
+//     console.log(chosen);
 
-    for (var i = 0; i < forms.length; i++) {
-      if (chosen === forms[i].routeName) {
-        return res.json(forms[i]);
-      }
-    }
+//     for (var i = 0; i < forms.length; i++) {
+//       if (chosen === forms[i].routeName) {
+//         return res.json(forms[i]);
+//       }
+//     }
 
-    return res.json(false);
-  }
-  return res.json(forms);
-});
+//     return res.json(false);
+//   }
+//   return res.json(forms);
+// });
 
-// Create New Characters - takes in JSON input
-app.post("/api/new", function(req, res) {
-  var newcharacter = req.body;
-  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+// // Create New Characters - takes in JSON input
+// app.post("/api/new", function(req, res) {
+//   var newcharacter = req.body;
+//   newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(newcharacter);
+//   console.log(newcharacter);
 
-  characters.push(newcharacter);
+//   characters.push(newcharacter);
 
-  res.json(newcharacter);
-});
+//   res.json(newcharacter);
+// });
+
